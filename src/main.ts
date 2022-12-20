00351d8aa3f7c32404bf3a7678a76716d173a4d2
henrique monteiro <henriquemonteiro037@gmail.com>
2022-12-20 00:18:29 +0000

    app finished

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,30 @@
 import './styles/global.scss'
 import './styles/main.scss'
+
+const accordionItems = document.querySelectorAll<HTMLDivElement>('[data-element="accordion-item"]')
+
+accordionItems.forEach((item, index) => {
+    const toggleButton = item.querySelector<HTMLButtonElement>('header button')
+
+    toggleButton?.addEventListener('click', () => {
+        item.classList.toggle('open')
+
+        closeAccordionItems(index)
+
+        if (item.classList.contains('open')) {
+            toggleButton.querySelector('i')
+                ?.setAttribute('class', 'ph-caret-down')
+        } else {
+            toggleButton.querySelector('i')
+                ?.setAttribute('class', 'ph-caret-right')
+        }
+    })
+})
+
+function closeAccordionItems(position: number) {
+    accordionItems.forEach((item, index) => {
+        if (position !== index) {
+            item.classList.remove('open')
+        }
+    })
+} 
